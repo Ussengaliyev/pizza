95afaf24dc1a4f0659ae9fca26256ac67a7e332c
Martyn <usengalibeka@gmail.com>
2023-07-04 17:26:12 +0600

    65

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className ="container">
           <div className ="content__top">
             < Categories />
-            < Sort />
+            < Sort   />
           </div>
           <h2 className ="content__title">Все пиццы</h2>
           <div className ="content__items">
