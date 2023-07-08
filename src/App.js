7d1ed110e24264c7bba98c1547bad4aab91c90e4
Martyn <usengalibeka@gmail.com>
2023-07-09 02:53:52 +0600

    connected Skeleton and Browser Router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
 import React from 'react';
+
 import './scss/app.scss';
 import Header from './components/header';
-import Categories from './components/categories';
-import Sort from './components/sort';
-import PizzaBlock from './components/PizzaBlock';
+import { Home } from './pages/Home';
+import NotFound from './pages/NotFound';
+import { Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart';
+
 
 
 
 function App() {
-  const [items, setItems] = React.useState([])
-
-  React.useEffect(() => {
-    fetch('https://64a69e93096b3f0fcc80230a.mockapi.io/items')
-      .then((response) => response.json())
-      .then((arr) => {
-        setItems(arr)
-      })
-  }, [])
-
-
+  
   return (
     <div className ="wrapper">
       < Header />
       <div className ="content"> 
         <div className ="container">
-          <div className ="content__top">
-            < Categories />
-            < Sort />
-          </div>
-          <h2 className ="content__title">Все пиццы</h2>
-          <div className ="content__items">
-            {items.map((obj) => (
-                < PizzaBlock {...obj} />
-              ))}
-                </div>
-              </div>
-            </div>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path = "*" element= {<NotFound/>}/>
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
 
